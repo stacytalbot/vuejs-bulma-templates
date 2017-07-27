285425778bcda39d46e2096fe514fa4bbec5282e
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-07-27 15:58:00 +0100

    Menu

---
+++ app/javascript/components/menu/MenuSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p class="menu-label">{{ name }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    'name': 'menu-section',
+
+    props: {
+      name: { required: true }
+    }
+  }
+</script>
